bb936fc741655299cb72f5e7908e20d5204dffc3
Borna Maticic <bornamaticic@yahoo.com>
2018-10-23 15:31:15 +0100

    finished test number,adding and removing

--- bus.rb
+++ bus.rb
@@ -1,10 +1,11 @@
 class Bus
 
-  attr_reader :route_number, :destination
+  attr_reader :route_number, :destination, :passengers
 
-  def initialize(route_number, destination)
+  def initialize(route_number, destination, passengers)
     @route_number = route_number
     @destination = destination
+    @passengers = []
   end
 
   def route_number
@@ -15,8 +16,21 @@ class Bus
     return "Ocean Terminal"
   end
 
-def drive_method
-  return "Brum brum"
-end
+  def drive_method
+    return "Brum brum"
+  end
+
+  def test_number_of_passengers
+    return @passengers
+  end
+
+  def add_person_to_passengers(passenger)
+    @passengers << passenger
+  end
+
+  def remove_person_from_passengers(passenger)
+    @passengers.delete(passenger)
+  end
+
 
 end
--- specs/bus_spec.rb
+++ specs/bus_spec.rb
@@ -1,11 +1,15 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../bus')
+require_relative('../person')
 
 class BusTest < Minitest::Test
 
   def setup
-    @bus1 = Bus.new("22", "Ocean Terminal")
+    @bus1 = Bus.new("22", "Ocean Terminal",[])
+    @person1 = Person.new("Kevin", 36)
+    @person2 = Person.new("Bill", 40)
+
   end
 
 
@@ -26,5 +30,25 @@ class BusTest < Minitest::Test
     actual = @bus1.drive_method
     assert_equal(expected, actual)
   end
+
+  def test_number_of_passengers
+    expected = []
+    actual = @bus1.passengers
+    assert_equal(expected, actual)
+  end
+
+  def test_add_person_to_passengers
+    expected = 1
+    @bus1.add_person_to_passengers(@person1)
+    actual = @bus1.passengers.count
+    assert_equal(expected, actual)
+  end
+
+  def test_remove_person_from_passengers
+    expected = 0
+    @bus1.remove_person_from_passengers(@person1)
+    actual = @bus1.passengers.count
+    assert_equal(expected, actual)
+  end 
 
 end
